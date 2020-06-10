30ea2424f0a7391a1b96c9d0d64e2ca99a37923d
roy <fengmushu@gmail.com>
2020-06-10 14:36:37 +0800

    or nand switch supported

--- make.sh
+++ make.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
-#make mt7621_nand_ax_rfb_defconfig  #(NAND flash)
-make mt7621_ax_rfb_defconfig  #(NOR flash)
-make CROSS_COMPILE=/opt/buildroot-gcc492_mips_glibc/usr/bin/mipsel-linux-
+
+build_nand()
+{
+	make mt7621_nand_ax_rfb_defconfig  #(NAND flash)
+	make CROSS_COMPILE=/opt/buildroot-gcc492_mips_glibc/usr/bin/mipsel-linux-
+}
+
+build_nor()
+{
+	make mt7621_ax_rfb_defconfig  #(NOR flash)
+	make CROSS_COMPILE=/opt/buildroot-gcc492_mips_glibc/usr/bin/mipsel-linux-
+}
+
+eval "build_$1" && {
+	mv u-boot-mt7621.bin u-boot-mt7621-$1.bin
+}
+
