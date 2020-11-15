3a6e381abd2292c413caf815c7a02886f74c78b6
fengmushu <fengmushu@gmail.com>
2020-11-15 12:35:06 +0800

    make.sh: split compile with def_config

--- make.sh
+++ make.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
+build_compile()
+{
+	make CROSS_COMPILE=/opt/buildroot-gcc492_mips_glibc/usr/bin/mipsel-linux-
+}
 
 build_nand()
 {
 	make mt7621_nand_ax_rfb_defconfig  #(NAND flash)
-	make CROSS_COMPILE=/opt/buildroot-gcc492_mips_glibc/usr/bin/mipsel-linux-
+	build_compile
 }
 
 build_nor()
 {
 	make mt7621_ax_rfb_defconfig  #(NOR flash)
-	make CROSS_COMPILE=/opt/buildroot-gcc492_mips_glibc/usr/bin/mipsel-linux-
+	build_compile
 }
 
 eval "build_$1" && {
